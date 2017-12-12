4b77f692b2e833f680cd0c60c5a276091383ac92
Yuya Takahashi <einsdrei2@gmail.com>
2017-12-13 04:11:46 +0900

    fix: Stop builds in executor-jenkins (#35)
    * fix: use a correct executor to stop a job
    
    * fix: change buildConfig.buildId to const
    
    * fix: refactor stop function
    
    * fix: add information for redis.hget

--- test/jobs.test.js
+++ test/jobs.test.js
@@ -139,23 +139,61 @@ describe('Jobs Unit Test', () => {
         );
 
         it('stops a job', () => {
-            const expectedConfig = { buildConfig: 'buildConfig' };
+            const expectedConfig = {
+                annotations: {
+                    'beta.screwdriver.cd/executor': 'k8s'
+                },
+                buildId: 8609,
+                container: 'node:4',
+                apiUri: 'http://api.com',
+                token: 'asdf'
+            };
 
             mockExecutor.stop.resolves(null);
+            mockRedisObj.hget.resolves(JSON.stringify(expectedConfig));
             mockRedisObj.hdel.resolves(1);
 
-            return jobs.stop.perform(expectedConfig, (err, result) => {
+            return jobs.stop.perform({ buildId: expectedConfig.buildId }, (err, result) => {
                 assert.isNull(err);
                 assert.isNull(result);
 
+                assert.calledWith(mockRedisObj.hget, 'buildConfigs', expectedConfig.buildId);
                 assert.calledWith(mockRedisObj.hdel, 'buildConfigs', expectedConfig.buildId);
-                assert.calledWith(mockExecutor.stop, expectedConfig);
+                assert.calledWith(mockExecutor.stop, {
+                    annotations: expectedConfig.annotations,
+                    buildId: expectedConfig.buildId
+                });
+            });
+        });
+
+        it('stop a build anyway when redis fails to get a config', () => {
+            const expectedConfig = {
+                annotations: {
+                    'beta.screwdriver.cd/executor': 'k8s'
+                },
+                buildId: 8609,
+                container: 'node:4',
+                apiUri: 'http://api.com',
+                token: 'asdf'
+            };
+            const expectedError = new Error('hget error');
+
+            mockExecutor.stop.resolves(null);
+            mockRedisObj.hget.rejects(expectedError);
+
+            return jobs.stop.perform({ buildId: expectedConfig.buildId }, (err, result) => {
+                assert.isNull(err);
+                assert.isNull(result);
+
+                assert.calledWith(mockRedisObj.hget, 'buildConfigs', expectedConfig.buildId);
+                assert.calledWith(mockExecutor.stop, { buildId: expectedConfig.buildId });
             });
         });
 
         it('returns an error from stopping executor', () => {
             const expectedError = new Error('executor.stop Error');
 
+            mockRedisObj.hget.resolves('{}');
             mockRedisObj.hdel.resolves(1);
             mockExecutor.stop.rejects(expectedError);
 
@@ -167,6 +205,7 @@ describe('Jobs Unit Test', () => {
         it('returns an error when redis fails to remove a config', () => {
             const expectedError = new Error('hdel error');
 
+            mockRedisObj.hget.resolves('{}');
             mockRedisObj.hdel.rejects(expectedError);
 
             return jobs.stop.perform({}, (err) => {
